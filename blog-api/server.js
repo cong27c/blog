7dae1861aa8ce19d3b20346357fdfe0124e005f6
Nguyễn Văn Công <nguyenvancongcbg27@hail.com>
2025-08-15 16:33:01 +0700

    update

--- blog-api/server.js
+++ blog-api/server.js
@@ -16,7 +16,7 @@ const { sequelize } = require("./src/models");
 const app = express();
 
 //Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://cblog.io.vn", credentials: true }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
